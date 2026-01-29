95ffe745e42e4b51db6c9605d5270e72cf9782c3
Laud Charles Ochei <laud.ochei@gmail.com>
2026-01-29 00:40:29 +0000

    first commit

--- employeesystem/settings.py
+++ employeesystem/settings.py
@@ -171,8 +171,16 @@ TIME_ZONE = 'Europe/London'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+#STATIC_URL = 'static/'
+#STATICFILES_DIRS = [BASE_DIR / 'static']
+
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
